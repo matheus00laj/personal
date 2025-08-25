4638d2b4fd4f18d5d69ba2321e9e99b353f07a56
Matheus Jose Andrade Lima <matheus00laj@gmail.com>
2025-08-25 08:10:22 -0300

    Adição das gems: rspec; bundler-audit; Atualização de gems com vulnerabilidades: activestorage; activerecord; Correção das ofenses presentes no rubocop.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,15 @@
 class PostsController < ApplicationController
-  allow_unauthenticated_access only: [:index, :show]
+  allow_unauthenticated_access only: [ :index, :show ]
   # before action é uma forma de compartilhar codigo entre ações.
   # neste caso, show, edit e update tinham o mesmo codigo: "@post = Post.find(params[:id])
   # a before action existe para que não precisemos repetir o mesmo código
   # em diferentes lugares, alem de que a before action é executada
   # antes mesmo das ações, o que previne alguns erros, já que show, edit e update
   # necessitam de registros existentes no banco de dados
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    # @posts é nossa variável de instanica que faz a querry de 
+    # @posts é nossa variável de instanica que faz a querry de
     # selecionar todos os registros do banco de dados!
     @posts = Post.all
   end
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   # path para o formulario de edição de uma postagem, utilizando ID como parametro
   def edit
   end
-  
+
   # query para edição do registro no banco de dados, utilizando o ID como parametro
   def update
     if @post.update(post_params)
@@ -58,7 +58,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.expect(post: [:title, :content, :subtitle, :link])
+    params.expect(post: [ :title, :content, :subtitle, :link ])
   end
-  
 end
